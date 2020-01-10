fa43eca39a5e83d195335d423482fe7d610bb87f
Marco Antonio <masjr@amazon.com>
2020-01-10 14:24:24 -0300

    FEA: add module resolvers

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
 
-import App from './App';
+import App from 'src/App';
 
 var mountNode = document.getElementById("root")
 ReactDOM.render(<App />, mountNode)
