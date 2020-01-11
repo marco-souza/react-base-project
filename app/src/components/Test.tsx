20843c5bdab6add23093b73aedd9cef66157e957
Marco Antônio <ma.souza.junior@gmail.com>
2020-01-11 00:41:25 -0300

    ENH: show loading status

--- app/src/components/Test.tsx
+++ app/src/components/Test.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react'
+import useLoading from 'src/redux/loading'
 
 const Test = () => {
+  const { state, actions } = useLoading()
+  const { isLoading } = state
+  const { startLoading, stopLoading } = actions
+
+  const toggleLoading = () => (isLoading ? stopLoading() : startLoading())
+
   return (
     <>
+      <div>{isLoading ? 'Loading' : 'Not loading'}</div>
       <div>Just a test component</div>
+      <button onClick={toggleLoading}>toggle</button>
     </>
   )
 }
